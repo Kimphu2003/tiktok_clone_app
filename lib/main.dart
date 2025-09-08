c2213eee40cb32f6275e4bd0a3846b5a53bab44a
Kimphu2003 <kimphu.tran154@gmail.com>
2025-09-08 13:31:35 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone_app/constants.dart';
+import 'package:tiktok_clone_app/views/screens/auth/login_screen.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tiktok Clone App',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
+
